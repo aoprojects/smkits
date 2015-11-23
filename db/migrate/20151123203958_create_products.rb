cd9a173ece6fd6fcac3922ac2a50eb9286b84f2a
Alison Ostendorf <aostendorf@gmail.com>
2015-11-23 17:05:10 -0500

    fixed migration and migrated products to database

--- db/migrate/20151123203958_create_products.rb
+++ db/migrate/20151123203958_create_products.rb
@@ -5,9 +5,6 @@ class CreateProducts < ActiveRecord::Migration
       t.string	:contents
       t.string	:details
       t.integer	:price
-      t.attachment :image_1
-      t.attachment :image_2
-      t.attachment :image_3
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20151123203958) do
+
+  create_table "products", force: :cascade do |t|
+    t.string   "product_name"
+    t.string   "contents"
+    t.string   "details"
+    t.integer  "price"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
 end
